a53659bad87c69255bdbd5f1987ccdd47f8b8d7e
Lucas Muniz <lucaslima1048@gmail.com>
2024-10-30 20:39:55 -0300

    Fisrt commit

---
+++ demo/src/main/java/com/post_graduation/PostGraduationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.post_graduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostGraduationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostGraduationSystemApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/post_graduation/PostGraduationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.post_graduation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostGraduationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
